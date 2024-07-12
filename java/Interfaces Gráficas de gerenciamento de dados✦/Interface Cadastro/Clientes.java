c2e83dd1c770d7d2968955c03d36ea521a1dd8d8
Gustavo Cortez <jogosgustavo2005@gmail.com>
2024-07-12 17:17:35 -0300

    Update Clientes.java

--- java/Interfaces Gráficas de gerenciamento de dados✦/Interface Cadastro/Clientes.java
+++ java/Interfaces Gráficas de gerenciamento de dados✦/Interface Cadastro/Clientes.java
@@ -1,1 +1,18 @@
+package clientes;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Clientes {
+    
+    static List<Cadastro>clientes = new ArrayList<Cadastro>();
+    
+    static void inserirCliente(String nome){
+        Cadastro c = new Cadastro(nome);
+        clientes.add(c);
+    }
+
+    public static void main(String[] args) {
+          new Interface();
+    }
+}
